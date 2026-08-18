f6008bbec1d5518671c350c9f2ca6af4bfac328d
凉数中 <2791389901@qq.com>
2026-08-18 22:46:48 +0800

    更新 MainActivity.java

--- app/src/main/java/com/example/simpleapp/MainActivity.java
+++ app/src/main/java/com/example/simpleapp/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.simpleapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.Gravity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView tv = new TextView(this);
         tv.setText("Hello World!\n应用启动成功！");
         tv.setTextSize(24);
-        tv.setGravity(android.view.Gravity.CENTER);
+        tv.setGravity(Gravity.CENTER);
         setContentView(tv);
     }
 }
