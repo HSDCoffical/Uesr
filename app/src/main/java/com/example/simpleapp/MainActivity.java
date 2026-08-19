0da1e2e0a5a952a442e223a4f495ee2ef1ba30ea
凉数中 <2791389901@qq.com>
2026-08-20 01:00:41 +0800

    更新 MainActivity.java

--- app/src/main/java/com/example/simpleapp/MainActivity.java
+++ app/src/main/java/com/example/simpleapp/MainActivity.java
@@ -292,7 +292,7 @@ public class MainActivity extends Activity {
         topBar.setOrientation(LinearLayout.HORIZONTAL);
         topBar.setGravity(Gravity.CENTER_VERTICAL);
         // 修复：让内容在白色区域内
-        int topPadding = statusBarHeight * 2 - dpToPx(40);
+        int topPadding = statusBarHeight * 2 - dpToPx(45);
         topBar.setPadding(16, topPadding, 16, 16);
         topBar.setLayoutParams(new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
