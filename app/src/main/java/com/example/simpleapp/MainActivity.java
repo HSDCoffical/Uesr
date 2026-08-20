22361fa450c8ea2d74edc8006bbadabead4128b5
凉数中 <2791389901@qq.com>
2026-08-21 00:15:09 +0800

    更新 MainActivity.java

--- app/src/main/java/com/example/simpleapp/MainActivity.java
+++ app/src/main/java/com/example/simpleapp/MainActivity.java
@@ -164,7 +164,7 @@ public class MainActivity extends Activity {
         View statusBarView = new View(this);
         statusBarView.setLayoutParams(new FrameLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
-                statusBarHeight * 2.1
+                statusBarHeight * 2
         ));
         statusBarView.setBackgroundColor(Color.WHITE);
         mainLayout.addView(statusBarView);
