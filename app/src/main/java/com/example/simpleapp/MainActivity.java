93319f0e5e5823163a7881841b2378662b81b35a
凉数中 <2791389901@qq.com>
2026-08-21 00:32:45 +0800

    更新 MainActivity.java

--- app/src/main/java/com/example/simpleapp/MainActivity.java
+++ app/src/main/java/com/example/simpleapp/MainActivity.java
@@ -488,7 +488,7 @@ topBar.setPadding(16, topPadding, 16, 16);
     ImageView searchIcon = new ImageView(this);
     searchIcon.setImageResource(android.R.drawable.ic_menu_search);
     searchIcon.setColorFilter(Color.GRAY);
-    searchIcon.setPadding(8, 20, 8, 8);
+    searchIcon.setPadding(8, 45, 8, 8);
     LinearLayout.LayoutParams iconParams = new LinearLayout.LayoutParams(
             dpToPx(24), dpToPx(24)
     );
